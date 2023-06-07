15076ff305e1d54b0aa0c9ce1d558c2972e37fad
TheMThe <mahdi.rahimi.poor.ravari@gmail.com>
2023-06-07 14:30:51 +0430

    fix

--- fa_ir/CustomerPortal_Client.php
+++ fa_ir/CustomerPortal_Client.php
@@ -9,21 +9,21 @@
  * ***********************************************************************************/
 
 $languageStrings = Array(
-	'Home' => 'Home',
-	'More' => 'More',
-	'Profile' => 'Profile',
-	'Change Password' => 'Change Password',
-	'Logout' => 'Logout',
-	'Welcome to Portal' => 'Welcome to Portal',
-	'LBL_CREATE_TICKET' => 'New Ticket',
-	'LBL_OPEN_TICKETS' => 'View Open Tickets',
-	'LBL_ADD_DOCUMENT' => 'Upload a Document',
-	'What would you like to do ?' => 'What would you like to do?',
-	'_start for notify' => 'keys',
-	'Your support ends on' => 'Your support ends on',
-	'_start for charts' => 'keys',
-	'OpenTicketsByPriority' => 'Open Tickets By Priority',
-	'TicketsClosureTimeByPriority' => 'Tickets Closure Time By Priority',
-	'_start for widgets' => 'keys',
-	'Recent' => 'Recent'
+	'Home' => 'خانه',
+	'More' => 'بیشتر',
+	'Profile' => 'پروفایل',
+	'Change Password' => 'تغییر پسورد',
+	'Logout' => 'خروج',
+	'Welcome to Portal' => 'به درگاه خوش آمدید',
+	'LBL_CREATE_TICKET' => 'بلیط جدید',
+	'LBL_OPEN_TICKETS' => 'دیدن بلیط های باز',
+	'LBL_ADD_DOCUMENT' => 'بارگزاری اسناد',
+	'What would you like to do ?' => 'دوست دارید چه کار کنید؟',
+	'_start for notify' => 'کلید ها',
+	'Your support ends on' => 'پشتیبانی شما تمام میشود در',
+	'_start for charts' => 'کلید ها',
+	'OpenTicketsByPriority' => 'باز کردن بلیت ها طبق اولویت',
+	'TicketsClosureTimeByPriority' => 'بسته شدن بلیط ها طبق اولویب',
+	'_start for widgets' => 'کلید ها',
+	'Recent' => 'آخرین'
 );
