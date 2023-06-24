e6d65df7e5c4b868d76225c754ebcc40d908aa15
TheMThe <mahdi.rahimi.poor.ravari@gmail.com>
2023-06-24 11:49:53 +0430

    FIX

--- fa_ir/Settings/CustomerPortal.php
+++ fa_ir/Settings/CustomerPortal.php
@@ -1,88 +1,89 @@
 <?php
-/* +**********************************************************************************
- * The contents of this file are subject to the vtiger CRM Public License Version 1.1
+/*+***********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
  * ("License"); You may not use this file except in compliance with the License
- * The Original Code is: vtiger CRM Open Source
+ * The Original Code is:  vtiger CRM Open Source
  * The Initial Developer of the Original Code is vtiger.
  * Portions created by vtiger are Copyright (C) vtiger.
  * All Rights Reserved.
- * ***********************************************************************************/
+ *************************************************************************************/
 
 $languageStrings = array(
-	'CustomerPortal' => 'درگاه مشتری',
-	'LBL_PORTAL_DESCRIPTION' => 'تنظیم کردن مزایای درگاه مشتری',
+  'CustomerPortal' => 'پورتال مشترکین',
+  'LBL_PORTAL_DESCRIPTION' => 'تنظیم امکانات پورتال مشترکین',
 
-	'LBL_PRIVILEGES' => 'مزایا',
-	'LBL_DEFAULT_ASSIGNEE' => 'مامور پیشفرض',
-	'LBL_PORTAL_URL' => 'Portal Url',
+  'LBL_PRIVILEGES' => 'امکانات',
+  'LBL_DEFAULT_ASSIGNEE' => 'عامل پیش فرض',
+  'LBL_PORTAL_URL' => 'آدرس صفحه وب',
 
 	//Fields
-	'LBL_MODULE_NAME' => 'نام مدال',
-	'LBL_ENABLE_MODULE' => 'فعال کردن مدال',
-	'LBL_VIEW_ALL_RECORDS' => 'دیدن رکورد های شرکت',
+  'LBL_MODULE_NAME' => 'نام ماژول',
+  'LBL_ENABLE_MODULE' => 'فعال نمودن ماژول',
+  '  Enable' => 'فعال',
+  'LBL_VIEW_ALL_RECORDS' => 'مشاهده کل رکوردها ',
 
 	//Messages
-	'LBL_PREVILEGES_MESSAGE' => "امتیازات این کاربر در درگاه کاربری اش اضافه میشوند.",
-	'LBL_DEFAULT_ASSIGNEE_MESSAGE' => 'Tickets will be Assigned to the selected Assignee by the default Group/User from the Customer Portal.',
-	'LBL_PORTAL_URL_MESSAGE' => 'This is URL for the Portal where your contacts can login to submit/track tickets, access knowledge base and do more.',
+  'LBL_PREVILEGES_MESSAGE' => 'این قوانین برای پورتال مشترکین در نظر گرفته شد.',
+  'LBL_DEFAULT_ASSIGNEE_MESSAGE' => 'تیکت ها برای عامل تعیین شده در نظر گرفته شد.',
+  'LBL_PORTAL_URL_MESSAGE' => 'آدرس وب صفحه پورتال مشترکین',
 	'LBL_CONTACTS_ENABLE_MESSAGE' => 'Contacts will be sent the login details when Portal access is enabled from Contact details page.',
 	'LBL_SUPPORT_NOTIFY_MESSAGE' => 'Support contract renewal alert is notified on the customer portal based on the number of days entered.',
-	'LBL_DRAG_AND_DROP_MESSAGE' => 'Drag and Drop modules to reorder in the Customer Portal',
+  'JS_PORTAL_INFO_SAVED' => 'تنظیمات پورتال مشتریان ذخیره شد',
 	//Portal shortcuts
-	'LBL_ADD_DOCUMENT' => 'Add Document',
-	'LBL_OPEN_TICKETS' => 'Open Tickets',
-	'LBL_CREATE_TICKET' => 'Create Ticket',
-	'LBL_ADD_SHORTCUT' => 'Add Shortcut',
-	'LBL_CREATE_CASE' => 'Create Case',
-	'LBL_OPEN_CASES' => 'Open Cases',
+	'LBL_ADD_DOCUMENT' => 'افزودن سند',
+	'LBL_OPEN_TICKETS' => 'تیکت های باز',
+	'LBL_CREATE_TICKET' => 'ایجاد تیکت',
+	'LBL_ADD_SHORTCUT' => 'افزودن میانبر',
+	'LBL_CREATE_CASE' => 'ایجاد مورد',
+	'LBL_OPEN_CASES' => 'موارد باز',
 	//Charts
 	'OpenTicketsByPriority' => 'Open tickets by priority',
 	'TicketsClosureTimeByPriority' => 'Tickets closure time by priority',
-	'OpenTicketsBySeverity' => 'Open tickets by severity',
 	'TicketsClosureTimeBySeverity' => 'Tickets closure time by severity',
 	'OpenCasesByPriority' => 'Open cases by priority',
 	'CasesClosureTimeByPriority' => 'Cases resolution time by priority',
-	'Announcement' => 'Announcement',
-	'Activity Stream' => 'Activity Stream',
-	'Fields and privilege' => 'Fields and privilege',
+	'Announcement' => 'اطلاعیه',
+	'Activity Stream' => 'جریان فعالیت',
+	'Fields and privilege' => 'فیلدها ودسترسی ها',
 	//Record visiblity status
-	'onlymine' => 'Show only  %s  linked to contact',
-	'all' => 'Show %s linked to contact and organization',
-	'products_or_services' => 'Show all %s',
-	'faq' => 'Show all published %s',
-	'LBL_LAYOUT_HEADER' => 'Portal Menu',
-	'LBL_HOME' => 'Home',
-	'LBL_CONTACT_NOTIFICATION' => 'Contact Support Notification',
-	'LBL_DAYS_BEFORE' => 'Days before',
-	'LBL_HOME_LAYOUT' => 'Portal Home Layout',
-	'LBL_RELATED_INFORMATION' => 'Related Information',
-	'LBL_RECORD_VISIBILITY' => 'Records Visibility',
-	'LBL_ADD_FIELDS' => 'Add Fields',
-	'LBL_READ_AND_WRITE' => 'Read and write',
-	'LBL_READ_ONLY' => 'Read only',
-	'LBL_PORTAL_FIELDS_PRIVILEGES' => 'Portal Fields And Privileges',
-	'LBL_RECENT' => 'Recent',
-	'LBL_REC_WIDGET' => 'Record Widget',
-	'LBL_CHARTS' => 'Charts',
-	'LBL_ANNOUNCEMENT' => 'Announcement',
-	'LBL_SHORTCUTS' => 'Shortcuts',
-	'LBL_SHOW' => 'Visible',
-	'History' => 'Updates',
-	'LBL_VISIBILITY' => 'Visibility',
-	'LBL_RECORD_PERMISSIONS' => 'Record Permissions',
-	'LBL_CREATE_RECORD' => 'Create Record',
-	'LBL_EDIT_RECORD' => 'Edit Record'
+	'onlymine' => 'فقط %s مرتبط با مخاطب را نشان دهید',
+	'all' => 'نمایش %s مرتبط با مخاطب و سازمان',
+	'products_or_services' => 'نمایش همه %s مورد',
+	'faq' => 'نمایش همه %s مورد منتشر شده',
+	'LBL_LAYOUT_HEADER' => 'منو پورتال',
+	'LBL_HOME' => 'صفحه نخست',
+	'LBL_CONTACT_NOTIFICATION' => 'با پشتیبانی تماس بگیرید',
+	'LBL_DAYS_BEFORE' => 'روز قبل',
+	'LBL_HOME_LAYOUT' => 'نمای صفحه نخست پورتال',
+	'LBL_RELATED_INFORMATION' => 'اطلاعات مرتبط',
+	'LBL_RECORD_VISIBILITY' => 'رکوردهای قابل نمایش',
+	'LBL_ADD_FIELDS' => 'افزودن فیلد',
+	'LBL_READ_AND_WRITE' => 'خواندن و نوشتن',
+	'LBL_READ_ONLY' => 'خواندن',
+	'LBL_PORTAL_FIELDS_PRIVILEGES' => 'فیلدها و دسترسی های پورتال',
+	'LBL_RECENT' => 'اخیر',
+	'LBL_REC_WIDGET' => 'ویجت رکورد',
+	'LBL_CHARTS' => 'نمودارها',
+	'LBL_ANNOUNCEMENT' => 'اطلاعیه',
+	'LBL_SHORTCUTS' => 'میانبرها',
+	'LBL_SHOW' => 'قابل مشاهده',
+	'History' => 'بروزرسانی ها',
+	'LBL_VISIBILITY' => 'قابل مشاهده',
+	'LBL_RECORD_PERMISSIONS' => 'سطح دسترسی رکورد',
+	'LBL_CREATE_RECORD' => 'ایجاد رکورد',
+	'LBL_EDIT_RECORD' => 'ویرایش رکورد',
+
 );
 
 $jsLanguageStrings = array(
-	'JS_PORTAL_INFO_SAVED' => 'Customer Portal settings saved',
-	'LBL_ADD_DOCUMENT' => 'Add Document',
-	'LBL_OPEN_TICKETS' => 'Open Tickets',
-	'LBL_CREATE_TICKET' => 'Create Ticket',
-	'JS_SELECT_SHORTCUT' => 'Select a shortcut',
-	'JS_ADD_FIELD' => 'Add fields',
-	'JS_SHOW' => 'Visible',
-	'JS_HIDE' => 'Hidden',
+  'JS_PORTAL_INFO_SAVED' => 'تنظیمات پورتال مشتریان ذخیره شد',
+	'LBL_ADD_DOCUMENT' => 'افزودن سند',
+	'LBL_OPEN_TICKETS' => 'تیکت های باز',
+	'LBL_CREATE_TICKET' => 'ایجاد تیکت',
+	'JS_SELECT_SHORTCUT' => 'افزودن میانبر',
+	'JS_ADD_FIELD' => 'افزودن فیلد',
+	'JS_SHOW' => 'قابل مشاهده',
+	'JS_HIDE' => 'مخفی کردن',
 	'JS_NUMBERS' => 'Accepts only positive whole number with upto four digits.',
 	'JS_MANDATORY_FIELDS_MISSING' => 'Fields could not be saved.Please select all mandatory fields.'
 );
